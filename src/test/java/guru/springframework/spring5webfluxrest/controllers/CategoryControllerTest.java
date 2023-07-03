78b1a2aca597a299465375380d91fbbd4d024be6
Caio-Santana <caio.santana@live.com>
2023-07-03 19:34:04 -0300

    Add Category Controller Test

---
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,60 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+
+public class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+    @Before
+    public void setUp() throws Exception {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+
+        categoryController = new CategoryController(categoryRepository);
+
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    public void getAllCategories() {
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(
+                        Category.builder().description("Nuts").build(),
+                        Category.builder().description("Eggs").build()
+                ));
+
+        webTestClient.get()
+                .uri(CategoryController.BASE_URL)
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getCategoryById() {
+        String id = "123";
+
+        BDDMockito.given(categoryRepository.findById(anyString()))
+                .willReturn(Mono.just(Category.builder().description("Fruits").build()));
+
+        webTestClient.get()
+                .uri(CategoryController.BASE_URL + "/" + id)
+                .exchange()
+                .expectBody(Category.class);
+
+    }
+}
